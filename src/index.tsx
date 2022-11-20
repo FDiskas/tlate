744266aefc9eb7ebeb07b0a3fa5337e271556449
Vytenis Kuciauskas <projektas@gmail.com>
2022-11-20 22:57:40 +0200

    keep alive :D

--- src/index.tsx
+++ src/index.tsx
@@ -1,26 +1,11 @@
 /* tslint:disable */
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-const loader = require('react-hot-loader');
-import 'antd/dist/antd.css';
+import 'antd/dist/reset.css';
 
 import App from './components/App';
 
 ReactDOM.render(
-    <loader.AppContainer>
-        <App/>
-    </loader.AppContainer>,
+        <App/>,
     document.getElementById('app')  as HTMLElement,
 );
-
-if (module.hot) {
-    module.hot.accept('./components/App', () => {
-        const NextApp = require('./components/App').default;
-        ReactDOM.render(
-            <loader.AppContainer>
-                <NextApp/>
-            </loader.AppContainer>,
-            document.getElementById('app')  as HTMLElement,
-        );
-    });
-}
--- webpack.config.js
+++
@@ -1,5 +0,0 @@
-require("ts-node").register({
-    filename: "config/webpack.config.ts",
-});
-
-module.exports = require("./config/webpack.config").default;
